10ad79c79ca2c9c045ff36be4c23a5ede31b63d5
Matt Briggs <brigmatt>
2019-11-23 15:49:32 -0800

    add-iterator-visualization

--- src/1063_valid_subarrays.cpp
+++ src/1063_valid_subarrays.cpp
@@ -1,9 +1,12 @@
 #include "catch.hpp"
+#include "run.h"
 #include <vector>
 #include <stack>
 #include <string>
 #include <sstream>
 #include <iostream>
+
+#if RUN==1063 || defined RUN_ALL
 
 // https://leetcode.com/problems/number-of-valid-subarrays/
 // Given an array A of integers, return the number of non-empty continuous subarrays that satisfy the following
@@ -12,9 +15,40 @@
 //
 // The intuiting behind this algorithm is extremely difficult to understand.
 //
-
-
 namespace valid_subarrays_1063 {
+
+    inline std::string printPosition(const std::vector<int>& values, int position) {
+        std::stringstream valueString;
+        std::stringstream arrowHead;
+        int idx = 0;
+        bool isFirst = true;
+        for(const auto& v : values) {
+            if(isFirst) {
+                valueString << "[";
+                arrowHead << " ";
+                isFirst = false;
+            } else {
+                valueString << ", ";
+                arrowHead << "  ";
+            }
+            std::stringstream temp;
+            temp << v;
+            const auto tempStr = temp.str();
+            const int sz = static_cast<int>(tempStr.size());
+            valueString << tempStr;
+            if(idx < position) {
+                for(int i = 0; i < sz; ++i) {
+                    arrowHead << " ";
+                }
+            } else if (idx == position) {
+                arrowHead << "^";
+            }
+            ++idx;
+        }
+        valueString << "]";
+        const std::string output = valueString.str() + "\n" + arrowHead.str();
+        return output;
+    }
 
     inline std::string printStack(std::stack<int> stk) {
         std::vector<int> values;
@@ -43,7 +77,9 @@ namespace valid_subarrays_1063 {
             int res = 0;
             int idx = 0;
             for(int n : nums) {
+                std::cout << "----------------------------------------------" << std::endl;
                 std::cout << "outer loop: idx=" << idx << ", n=" << n << ", stack: " << printStack(stk) << std::endl;
+                std::cout << printPosition(nums, idx) << std::endl;
                 while(!stk.empty() && stk.top() > n) {
                     stk.pop();
                     std::cout << "idx: "<< idx << ", n: " << n << ", stack popped: " << printStack(stk) << std::endl;
@@ -75,3 +111,5 @@ TEST_CASE("valid_subarrays_02") {
     const auto actual = s.validSubarrays(nums);
     CHECK( expected == actual );
 }
+
+#endif
---
+++ src/run.h
@@ -0,0 +1,2 @@
+#pragma once
+#define RUN 1063
